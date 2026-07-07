56adbcab46452b731c225249643292c8e13bd558
Arya-azimi <aryaazimi200@gmail.com>
2026-07-07 10:57:08 +0330

    debugg deployment

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutUsPage() {
+  return (
+    <main className="w-full min-h-screen py-24">
+      <div className="w-[90%] max-w-[1400px] mx-auto">
+        <h1 className="text-4xl font-bold">About Us</h1>
+      </div>
+    </main>
+  );
+}

de500dec6d93fbb6e48cd5071ceb744791738bf8
Arya-azimi <aryaazimi200@gmail.com>
2026-07-13 17:05:12 +0330

    a little bit smoother

---
+++ src/components/SmoothScrollProvider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ReactLenis } from "lenis/react";
+import { ReactNode } from "react";
+
+export default function SmoothScrollProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <ReactLenis root options={{ lerp: 0.07, smoothWheel: true }}>
+      {children as any}
+    </ReactLenis>
+  );
+}

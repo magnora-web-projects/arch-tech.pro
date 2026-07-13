57dd6798f8ab9fea52f15397349b47a361be6acb
Arya-azimi <aryaazimi200@gmail.com>
2026-07-13 17:07:03 +0330

    remove some covers

--- src/components/pages-components/home/00-LuxuryBackground.tsx
+++ src/components/pages-components/home/00-LuxuryBackground.tsx
@@ -133,17 +133,6 @@ export default function LuxuryBackground() {
         ref={canvasRef}
         className="absolute inset-0 w-full h-full mix-blend-screen opacity-60"
       />
-
-      <div className="absolute inset-0 bg-[radial-gradient(circle_at_50%_50%,transparent_0%,#050505_95%)] opacity-90" />
-
-      <div className="absolute inset-0 bg-gradient-to-r from-[#050505]/80 via-transparent to-[#050505]/30" />
-
-      <div
-        className="absolute inset-0 opacity-[0.03] mix-blend-overlay"
-        style={{
-          backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
-        }}
-      />
     </div>
   );
 }
